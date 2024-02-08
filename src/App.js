488b3488e4da02b8dcb6bb680100f4f45c0f833a
KoniChivaJS <mredik200@gmail.com>
2024-02-08 18:06:37 +0200

    Add posts & editing & users

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ import {
   Redirect,
 } from 'react-router-dom'
 
+import { PostsList } from './features/posts/PostsList';
+import { AddPostForm } from './features/posts/AddPostForm'
+import { SinglePostPage } from './features/posts/SinglePostPage'
+import { EditPostForm } from './features/posts/EditPostForm'
+
+
 import { Navbar } from './app/Navbar'
 
-import { PostsList } from './features/posts/PostsList'
-import { AddPostForm } from './features/posts/AddPostForm'
 function App() {
   return (
     <Router>
@@ -20,12 +24,15 @@ function App() {
             exact
             path="/"
             render={() => (
-              <React.Fragment>
-                <AddPostForm />
+              <>
                 <PostsList />
-              </React.Fragment>
+                <AddPostForm />
+              </>
             )}
           />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
+
           <Redirect to="/" />
         </Switch>
       </div>
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,19 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { nanoid } from '@reduxjs/toolkit'
+
 
 const initialState = [
   { id: '1', title: 'First Post!', content: 'Hello!' },
-  { id: '2', title: 'Second Post', content: 'More text' },
+  { id: '2', title: 'Second Post', content: 'More text' }
 ]
 
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    postAdded(state, action) {
-      state.push(action.payload)
-    },
-  },
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload)
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+            user: userId
+          }
+        }
+      }
+    }
+  }
 })
-export const { postAdded } = postsSlice.actions
+
+export const { postAdded, postUpdated } = postsSlice.actions
 
 export default postsSlice.reducer
